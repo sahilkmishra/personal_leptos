848094ff896aebcc1bcbfcb951f50327afcb7ba3
Sahil Mishra <slymishra@gmail.com>
2025-07-29 17:54:51 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,6 @@ fn App(increment: i32) -> impl IntoView {
     view! {
     <div class="container">
 
-            <picture>
-                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
-                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
-            </picture>
-
         <h1>"Welcome to Leptos"</h1>
         <h2><i>"On Github Pages"</i></h2>
 
